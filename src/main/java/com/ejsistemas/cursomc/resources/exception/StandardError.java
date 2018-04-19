c3daab7e433bbab170faa15fb5e62fa33381a13b
EdenilsonJak <edenilsonjak@gmail.com>
2018-04-19 10:09:27 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/ejsistemas/cursomc/resources/exception/StandardError.java
@@ -0,0 +1,42 @@
+package com.ejsistemas.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer status;
+	private String msg;
+	private Long timesStamp;
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Long getTimesStamp() {
+		return timesStamp;
+	}
+
+	public void setTimesStamp(Long timesStamp) {
+		this.timesStamp = timesStamp;
+	}
+
+	public StandardError(Integer status, String msg, Long timesStamp) {
+		this.status = status;
+		this.msg = msg;
+		this.timesStamp = timesStamp;
+	}
+		
+}
--- src/main/java/com/ejsistemas/cursomc/services/CategoriaService.java
+++ src/main/java/com/ejsistemas/cursomc/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.ejsistemas.cursomc.domain.Categoria;
 import com.ejsistemas.cursomc.repositories.CategoriaRepository;
+import com.ejsistemas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	CategoriaRepository repo;
-	
-	public Categoria buscar(Integer id){
+
+	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
