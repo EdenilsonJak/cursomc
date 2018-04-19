f08fe50e82eab700dc0cbd06918df14ffe7a1720
EdenilsonJak <edenilsonjak@gmail.com>
2018-04-19 11:59:22 -0300

    Estado e Cidade

---
+++ src/main/java/com/ejsistemas/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.ejsistemas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ejsistemas.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+	
+	
+}
