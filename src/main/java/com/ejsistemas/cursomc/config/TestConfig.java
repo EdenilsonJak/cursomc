3cece785b2581138dc406b7829801827ef4143db
EdenilsonJak <edenilsonjak@gmail.com>
2018-05-03 19:14:33 -0300

    MockEmailService com Logger. Padroes strategy e Template Method

--- src/main/java/com/ejsistemas/cursomc/config/TestConfig.java
+++ src/main/java/com/ejsistemas/cursomc/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.ejsistemas.cursomc.services.DBService;
+import com.ejsistemas.cursomc.services.EmailService;
+import com.ejsistemas.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -20,6 +22,11 @@ public class TestConfig {
 	public boolean instantiateDatabase() throws ParseException{
 		dbService.instanteateTestDatabase();
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService(){
+		return new MockEmailService();
 	}
 	
 }
---
+++ src/main/java/com/ejsistemas/cursomc/services/AbstractEmailService.java
@@ -0,0 +1,31 @@
+package com.ejsistemas.cursomc.services;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+
+import com.ejsistemas.cursomc.domain.Pedido;
+
+public abstract class AbstractEmailService implements EmailService{
+
+	@Value("${default.sender}")
+	private String sender;
+	
+	@Override
+	public void sendOrderConfirmationEmail(Pedido obj){
+		SimpleMailMessage sm = prepareSimpleMailMessegeFromPedido(obj);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareSimpleMailMessegeFromPedido(Pedido obj) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(obj.getCliente().getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Pedido confirmado: "+ obj.getId());
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText(obj.toString());
+		return sm;
+	}
+	
+}
